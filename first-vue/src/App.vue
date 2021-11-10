520d58905bb7ddd8c1cab1dbb04b3cc5fa1ceb74
lumilio <logistic-division@lumilium.com>
2021-11-10 22:32:38 +0100

    primo approccio con vue CLI

---
+++ first-vue/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <obj1/>
+  </div>
+</template>
+
+<script>
+/* import HelloWorld from './components/HelloWorld.vue' */
+import obj1 from './components/titolo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    /* HelloWorld */
+    obj1
+
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
